080f9d4843c7d4f9f2c0433c71ed646c985709fe
abollorodriguez <abollorodriguez@1DAM-17>
2023-11-29 09:13:30 +0100

    Mi "primer" commit

---
+++ src/ud2/Ejemplo1.java
@@ -0,0 +1,17 @@
+package ud2;
+class Ejemplo1 {
+	public static void main (String[] args) {
+		
+		int suma = 0;
+		
+		for (int i =0; i<1000; i++) {
+			System.out.println(i);
+			suma+=i;
+		}
+		
+		
+		System.out.println(suma);
+
+	}
+
+}
